c349de4d120ce71657e3bf238935d6ecac5269a0
xuxutu <1355124336@qq.com>
2021-05-07 23:36:52 +0800

    pthread

---
+++ C++/C/MultiThread/线程/src/th.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+
+static void cleanup_func(void *p)
+{
+	puts(p);
+}
+
+static void* func(void* p)
+{
+	
+	puts("Thread is working!");
+
+	pthread_cleanup_push(cleanup_func, "cleanup:1");
+	pthread_cleanup_push(cleanup_func, "cleanup:2");
+	pthread_cleanup_push(cleanup_func, "cleanup:3");
+
+
+	pthread_cleanup_pop(1);
+	pthread_cleanup_pop(1);
+	pthread_cleanup_pop(1);
+
+	pthread_exit(NULL);
+//	return NULL;
+//	_exit(1);
+}
+
+int main()
+{
+	pthread_t tid;
+	int err;
+
+	puts("Begin!");
+
+	err = pthread_create(&tid, NULL, func, NULL);
+	if(err)
+	{
+		fprintf(stderr, "pthread_create():%s\n", strerror(err));
+		exit(1);
+	}
+
+
+	pthread_join(tid, NULL);
+
+	puts("End!");
+
+
+
+	return 0;
+}
