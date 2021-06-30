a089b3f1bd08ef17ce35b7f5f02bfa6eeb9b68d2
xuxutu <1355124336@qq.com>
2021-06-30 16:38:34 +0800

    Daily build

---
+++ CSAPP/code/test.c
@@ -0,0 +1,8 @@
+
+void func()
+{
+	int a;
+	int *p = &a;
+	*p = 2;
+
+}
---
+++ CSAPP/code/test.s
@@ -0,0 +1,34 @@
+	.file	"test.c"
+	.text
+	.globl	func
+	.type	func, @function
+func:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	leaq	-20(%rbp), %rax
+	movq	%rax, -16(%rbp)
+	movq	-16(%rbp), %rax
+	movl	$2, (%rax)
+	nop
+	movq	-8(%rbp), %rax
+	xorq	%fs:40, %rax
+	je	.L2
+	call	__stack_chk_fail@PLT
+.L2:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	func, .-func
+	.ident	"GCC: (Ubuntu 7.5.0-3ubuntu1~18.04) 7.5.0"
+	.section	.note.GNU-stack,"",@progbits
