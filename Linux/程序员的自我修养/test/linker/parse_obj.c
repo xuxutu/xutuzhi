ca57ef8630fc29d1a070ed78d1e8c70782a4ed52
xuxutu <1355124336@qq.com>
2021-09-21 16:37:55 +0800

    Daily build

--- Linux/程序员的自我修养/test/linker/parse_obj.c
+++ Linux/程序员的自我修养/test/linker/parse_obj.c
@@ -50,9 +50,35 @@ void parser_elf_symtb(Elf64_Sym* symtb, char* buf, int c)
 	for(int i = 0; i < c; i++)
 	{
 		puts(buf + (symtb + i)->st_name);
+		printf("0x%lx\n", (symtb + i)->st_name);
 	}
 }
 
+int get_rel_index(Elf64_Shdr* sectb, char* buf, int c, int s)
+{
+
+	int flag = -1;
+	for(int i = 0; i < c; i++)
+	{
+		if((sectb + i)->sh_type == s)
+		{
+			printf("index = 0x%lX\n", i);
+			puts((sectb + i)->sh_name + buf);
+			flag = i;
+		}
+	}
+
+	return flag;
+}
+
+void parser_elf_reltb(Elf64_Rel* reltb, char* buf, int c)
+{
+	for(int i = 0; i < c; i++)
+	{
+		printf("重定位入口的偏移:0x%lX\n", reltb[i].r_offset);
+		printf("重定位入口的类型和符号:0x%lX\n", reltb[i].r_info);
+	}
+}
 
 int main(int argc, const char** argv)
 {
@@ -141,8 +167,46 @@ int main(int argc, const char** argv)
 
 	Elf64_Sym* symtb = (Elf64_Sym*)symtb_buf;
 
-	printf("符号表大小:%d, 每项大小:%ld\n", symtb_size, sizeof(Elf64_Sym));	
+	printf("符号表大小:%d, 每项大小:%ld | %d\n", symtb_size, sizeof(Elf64_Sym), ((Elf64_Shdr*)sectb_buf + sym_str_index)->sh_entsize);	
 	parser_elf_symtb(symtb, chartb_buf, symtb_size / sizeof(Elf64_Sym));
 
+	//获取可从定位表
+	//获取可重定位表的索引
+	puts("reltb");
+
+
+	int reltb_index = 2;
+	int reltb_size = ((Elf64_Shdr*)sectb_buf + reltb_index)->sh_size;
+	char reltb_buf[reltb_size];
+	lseek(fd, ((Elf64_Shdr*)sectb_buf + reltb_index)->sh_offset, SEEK_SET);	
+	if(read(fd, reltb_buf, reltb_size) != reltb_size)
+	{
+		perror("read error");
+		exit(1);
+	}
+	
+	Elf64_Rel* reltb = (Elf64_Rel*)reltb_buf;
+	parser_elf_reltb(reltb, symtb_buf, reltb_size / sizeof(Elf64_Rel));
+	printf("重定位表项数:%d %d\n", ((Elf64_Shdr*)sectb_buf + reltb_index)->sh_entsize, reltb_size / sizeof(Elf64_Rel));	
+/*
+	while((reltb_index = get_rel_index((Elf64_Shdr*)sectb_buf, secstr_buf, head->e_shnum, SHT_RELA)) != -1)
+	{
+		printf("可重定位表的索引:%d\n", reltb_index);
+		int reltb_size = ((Elf64_Shdr*)sectb_buf + reltb_index)->sh_size;
+		char reltb_buf[reltb_size];
+
+		lseek(fd, ((Elf64_Shdr*)sectb_buf + reltb_index)->sh_offset, SEEK_SET);	
+		if(read(fd, reltb_buf, reltb_size) != reltb_size)
+		{
+			perror("read error");
+			exit(1);
+		}
+		puts("-------------------------");
+		puts(((Elf64_Shdr*)sectb_buf + reltb_index)->sh_name + symtb_buf);
+		Elf64_Rel* reltb = (Elf64_Rel*)reltb_buf;
+		parser_elf_reltb(reltb, symtb_buf, reltb_size / sizeof(Elf64_Rel));
+		printf("重定位表项数:%d %d\n", ((Elf64_Shdr*)sectb_buf + reltb_index)->sh_entsize, reltb_size / sizeof(Elf64_Rel));
+	}
+*/
 	return 0;
 }
