30bf6cc0099160836dcbf27f8f3afa7324a51cf7
xuxutu <1355124336@qq.com>
2021-09-22 22:35:54 +0800

    Daily build

---
+++ CSAPP/csapp/css/cpu.h
@@ -0,0 +1,32 @@
+#ifndef CPU_H__
+#define CPU_H__
+
+#include <stdlib.h>
+#include <stdint.h>
+
+typedef struct CPU_STRUCT
+{
+	union
+	{
+		struct
+		{
+			uint8_t al;
+			uint8_t ah;
+		};
+		uint16_t ax;
+		uint32_t eax;
+		uint64_t rax;
+	};
+
+	uint64_t rbx;
+	uint64_t rcx;
+	uint64_t rdx;
+	uint64_t rsi;
+	uint64_t rdi;
+	uint64_t rbp;
+	uint64_t rsp;
+
+	uint64_t rip;
+} cpu_t;
+
+#endif
